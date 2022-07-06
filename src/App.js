24ef5ebdbb1ef38005c73d704ab6b568a7510261
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-07-06 14:09:09 +0500

    Generate array of 10 random numbers

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import React from 'react';
 import Die from './Die';
 import './index.css';
 
-const App = () => (
-  <main>
-    <div className='dice-container'>
-      <Die value="1" />
-      <Die value="2" />
-      <Die value="3" />
-      <Die value="4" />
-      <Die value="5" />
-      <Die value="6" />
-      <Die value="7" />
-      <Die value="8" />
-      <Die value="9" />
-      <Die value="10" />
-    </div>
-  </main>
-);
+const App = () => {
+  // new array to hold my numbers
+  // loop 10 times
+  // push a random number from 1-6 to my array
+  // return array
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i += 1) {
+      newDice.push(Math.ceil(Math.random()* 6) );
+    }
+    return newDice;
+  }
+  console.log(allNewDice());
 
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="7" />
+        <Die value="8" />
+        <Die value="9" />
+        <Die value="10" />
+      </div>
+    </main>
+  );
+};
 export default App;
--- src/Die.js
+++ src/Die.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Die = (props) => (
-  <div className='die-face'>
-    <h2 className='die-num'>{props.value}</h2>
+  <div className="die-face">
+    <h2 className="die-num">{props.value}</h2>
   </div>
 );
 
